7b79b74f06febbb04b63cd50e19a528b91b1b8ef
Geronimo Perez <geronimo.perez4@gmail.com>
2023-09-15 14:05:12 -0400

    fixed the apps links

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -10,13 +10,13 @@ const speech =
             title: 'Professional ReadMe Gen',
             message: `This application can help you generate profesional readme files for your projects`,
             images: ['mockup.png','mockup2.png','mockup3.png','mockup4.png','mockup5.png'],
-            website: 'ProfessionalReadmeGenerator'
+            website: 'https://analysisanddevelopment.com/ProfessionalReadmeGenerator'
         },
         { 
             title: 'U.S. Census Analysis Dashboard ',
             message: `How about if we obtain Census Data from a data bank like Kaggle and with the use of HTML, CSS and JavaScript libraries create an interactive website that would allows us to visually analyze correlation between location, obesity, income, helthcare, and smokers.`,
             images: ['D3-Projects.gif'],
-            website: '/US-Census-Analysis-Dashboard/'
+            website: 'https://analysisanddevelopment.com/US-Census-Analysis-Dashboard/'
         },
         {
             title: 'Tableau',
@@ -34,7 +34,7 @@ const speech =
             title: 'Latitude & Weather Analysis',
             message: `Or how about obtaining current weather of hundreds of cities around the world to conduct your own study about the effect of latitude on the climate and then displaying it on your own interactive website.`,
             images: ['web_1.png', 'web_2.png'],
-            website: '/Weather_And_Latitude_Analysis'
+            website: 'https://analysisanddevelopment.com/Weather_And_Latitude_Analysis'
         },
         {
             title: 'Employees Management Node Sql Js',
@@ -76,19 +76,19 @@ const speech =
             title: 'React Memory Game',
             message: `How about if we start by creating a single page react game application which reinforces memory power.  Which the objective of the game would be to click all images once as the change position in every click`,
             images: ['reactMemoryGame.png'],
-            website: '/react-memory-game/'
+            website: 'https://analysisanddevelopment.com/react-memory-game/'
         },
         {
             title: 'GenPass',
             message: `It's difficult to keep creating passwords to secure all our electronic interactions and remembering them when you need them.  Imagine creating a password management solution.`,
             images: ['genPass1.png','genPass2.png','genPass3.png','genPass4.png','genPass5.png','genPass6.png'],
-            website: '/Password-Generator/'
+            website: 'https://analysisanddevelopment.com/Password-Generator/'
         },
         {
             title: 'Spiritual Engineering',
             message: `How about if we strive for greatness and we provide the users more than a game.  We provide them with a spiritual experience like no other`,
             images: ['SpiritualEngineering.png'],
-            website: '/SpiritualEngineering/'
+            website: 'https://analysisanddevelopment.com/SpiritualEngineering/'
         },
         {
             title: 'Node Team Builder',
@@ -106,7 +106,7 @@ const speech =
             title: 'Weather Dashboard',
             message: `Let's create an application that provides a weather dashboard from which you can obtain current weather conditions for any city and a five days forecast, all from a public API.  This includes the UV index with a numeric value and danger color scale.`,
             images: ['weather.png'],
-            website: '/WeatherForecast'
+            website: 'https://analysisanddevelopment.com/WeatherForecast'
         },
         {
             title: 'Mission to Jupiter',
@@ -136,7 +136,7 @@ const speech =
             title: 'React Portfolio',
             message: `Imagine creating a react portfolio that showcase some of your created applications and provides contact information`,
             images: ['portfolio1.png','portfolio2.png','portfolio3.png','portfolio4.png','portfolio5.png','portfolio6.png','portfolio7.png'],
-            website: '/Portfolio/'
+            website: 'https://analysisanddevelopment.com/Portfolio/'
         },
         {
             title: 'LIRI a SIRI\'s simulation',
